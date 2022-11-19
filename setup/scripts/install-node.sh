49256730b968e40f224b4a396123303620236c27
cparson1 <cparson@gmail.com>
2022-11-19 14:07:06 -0500

    Update install-node.sh

--- setup/scripts/install-node.sh
+++ setup/scripts/install-node.sh
@@ -7,9 +7,6 @@ then
 
 else
 
-    # taken from the the official magic mirror installer script:
-    # https://github.com/MichMich/MagicMirror/blob/v2.10.1/installers/raspberry.sh
-
     cd ~/
 
     info "Installing node"
