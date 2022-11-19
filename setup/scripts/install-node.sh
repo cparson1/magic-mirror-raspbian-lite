ddf7dbed740ab741c2b5106b149160f8d994ecb5
cparson1 <cparson@gmail.com>
2022-11-19 14:06:52 -0500

    Update install-node.sh

--- setup/scripts/install-node.sh
+++ setup/scripts/install-node.sh
@@ -12,33 +12,10 @@ else
 
     cd ~/
 
-    NODE_MAJOR_VERSION="12"
-    ARM=$(uname -m)
-
-    if [[ "$ARM" = "armv7l" ]]
-    then
-        echo "Original 'ARM' value is: $ARM, so using 'armv6l'"
-        ARM="armv6l"
-    fi
-
     info "Installing node"
 
-    # node official debian distribution doesn't support ARM, so using unofficial distribution
-    sudo apt-get install -y --only-upgrade libstdc++6
-
-    node_ver=$(curl -sL https://unofficial-builds.nodejs.org/download/release/index.tab | grep $ARM | grep -m 1 v$NODE_MAJOR_VERSION | awk '{print $1}')
-    echo "Latest release in the $NODE_MAJOR_VERSION family for $ARM is $node_ver"
-
-    NODE_TAR_FILE="node_release-$node_ver.tar.gz"
-
-    curl -sL https://unofficial-builds.nodejs.org/download/release/$node_ver/node-$node_ver-linux-$ARM.tar.gz >"$NODE_TAR_FILE"
-    echo "using release tar file = '$NODE_TAR_FILE' and extracting to '/usr/local'"
-
-    cd /usr/local
-    sudo tar --strip-components 1 -xzf "$HOME_DIR/$NODE_TAR_FILE"
-    cd - >/dev/null
-
-    rm "$HOME_DIR/$NODE_TAR_FILE"
+    curl -fsSL https://deb.nodesource.com/setup_19.x | bash - &&\
+    sudo apt-get install -y nodejs
 
 fi
 
